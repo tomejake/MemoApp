7aa7547ba399a52a1f54b5700d33bd9fee18421d
tomejake <tomejake22@gmail.com>
2022-10-12 19:16:54 +0900

    글쓰기 UI

--- app/src/main/java/com/example/memoapp/WriteActivity.kt
+++ app/src/main/java/com/example/memoapp/WriteActivity.kt
@@ -3,9 +3,9 @@ package com.example.memoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_write)
     }
 }
